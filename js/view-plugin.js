434e523988cc39e35ede10063f74067bf47d8af3
Justin Yip <justin.yip@foxmail.com>
2014-05-25 11:59:12 +0800

    view插件的代理子类，只保存字符串template，在初始化的时候才生成DOM，解决之前每个实例指向同一个element的问题

--- js/view-plugin.js
+++ js/view-plugin.js
@@ -15,7 +15,14 @@ define(function () {
 		}, fail);
 	}
 
-	var loadViewClassByEL = function(require, el, success, fail){
+	var loadViewClassByEL = function(require, htmlTemplate, success, fail){
+		//只要body内的类容
+		htmlTemplate = (/<html/i.test(htmlTemplate)) ? htmlTemplate.match(/<body[^>]*>([\s\S.]*)<\/body>/i)[0] : htmlTemplate;
+		//转换成DOM
+		var el = document.createElement('div');
+		el.innerHTML = htmlTemplate;
+		el = el.firstElementChild;
+
 		//el的绑定类，若没有，默认为最普通的View（框架定义的）
 		var elementBinding = (el.getAttribute('data-window') || el.getAttribute('data-view') || '$view').replace('$', 'butterfly/');
 		//el子节点的绑定类集合
@@ -25,22 +32,27 @@ define(function () {
 		});
 		view_names.unshift(elementBinding);
 
-		//加载el的绑定类
+		//加载el以及el的子节点的所有绑定类
 		require(view_names, function(){
 
 			var TopViewClass = arguments[0];
 
 			var ProxyViewClass = TopViewClass.extend({
 
-				element: el,
+				template: htmlTemplate,
 
 				initialize: function(){
 					TopViewClass.prototype.initialize.call(this, arguments);
 					
-					var me = this;
+					//转换成DOM
+					var el = document.createElement('div');
+					el.innerHTML = this.template;
+					el = el.firstElementChild;
 					//replace element
-					this.setElement(this.element);
+					this.setElement(el);
 
+					//apply binding
+					var me = this;
 					_.chain(this.el.querySelectorAll('[data-view]'))
 					.foldl(function(mapping, node){
 						var binding = node.getAttribute('data-view').replace('$', 'butterfly/');
@@ -64,21 +76,16 @@ define(function () {
 		var me = this;
 		Butterfly.log('loadView: %s', view);
 		if (typeof view == 'string' && view.endsWith('html')) {
-			require(['text!'+view], function(page){
 
-				var el = document.createElement('div');
-				el.innerHTML = (/<html/i.test(page)) ? page.match(/<body[^>]*>([\s\S.]*)<\/body>/i)[0] : page;
-
-				loadViewClassByEL(require, el.firstElementChild, success, fail);
+			require(['text!'+view], function(template){
+				loadViewClassByEL(require, template, success, fail);
 			}, fail);
 
 		} else if (typeof view == 'string') {
-			require([view], function(View){
-				success(new View());
-			}, fail);
+			require([view], success, fail);
 
 		} else {
-			loadViewClassByEL(require, view ,success, fail);
+			throw new Error('view loader plugin require a view name of string type');
 		}
 	}
 
