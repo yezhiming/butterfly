c059c7e596e299a75c99bf88be1a2b6d1bce62a0
Justin Yip <justin.yip@foxmail.com>
2014-10-30 21:24:02 +0800

    fix jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('build', [
     'clean:dist',
